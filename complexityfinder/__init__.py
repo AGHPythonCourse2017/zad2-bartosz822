45e9e59ca21853b908f03916062526884b151785
Bartosz Radzynski <radzynskib@gmail.com>
2017-05-08 00:02:21 +0200

    fixing init

--- complexityfinder/__init__.py
+++ complexityfinder/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """Bartosz Radzyński"""
 __email__ = 'radzynskib@gmail.com'
 __version__ = '0.21.37'
+
+from complexityfinder import complexity_finder, arr_generator, simple_cleaner, simple_setup, test_it
