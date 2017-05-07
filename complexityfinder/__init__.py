fc831c15b067af1dc46d338f6c02a2337a4d996e
Bartosz Radzynski <radzynskib@gmail.com>
2017-05-08 00:08:02 +0200

    fixing version number inconsitency

--- complexityfinder/__init__.py
+++ complexityfinder/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Bartosz Radzyński"""
 __email__ = 'radzynskib@gmail.com'
-__version__ = '0.21.37'
+__version__ = '0.21.39'
 
 from complexityfinder import complexity_finder, arr_generator, simple_cleaner, simple_setup, test_it
